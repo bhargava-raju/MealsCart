8bf46c6bc51730c69bcdeb3e118d8549dd01e521
Bhargava Raju <BhargavaR@anthology.com>
2023-03-09 22:20:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {  
+  return (
+    <React.Fragment>
+     <h1>Introduction to React Scripting</h1>
+    </React.Fragment>
+  );
+}
+
+export default App;
